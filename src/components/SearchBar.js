1e1004fb9e883aae32de0eaf0b56fec66424bdd6
Shivangi Srivastava <shi06dec@gmail.com>
2024-02-28 14:26:33 -0700

    Hanlding form input by creating a controlled input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
+
 function SearchBar({ onSubmit }) {
-  const handleClick = () => {
-    onSubmit('cars');
+  const [term, setTerm] = useState("");
+  
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(term);
+  }
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
   }
 
   return (
     <div>
-      <input />
-      <button onClick={handleClick}>Click Me</button>
+      <form onSubmit={handleFormSubmit}>
+        <input value={term} onChange={handleChange} />
+      </form>
     </div>
   );
 }
