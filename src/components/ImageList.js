54f127ef191ce69ce99bfb46d2bfeb701f766cec
Shivangi Srivastava <shi06dec@gmail.com>
2024-02-28 17:57:12 -0700

    Add styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,4 +1,5 @@
 import ImageShow from './ImageShow';
+import './ImageList.css';
 
 function ImageList({ images }) {
   const renderedImages = images.map((image) => {
@@ -6,7 +7,7 @@ function ImageList({ images }) {
   });
 
   return (
-    <div>{renderedImages}</div>
+    <div className='image-list'>{renderedImages}</div>
   );
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './SearchBar.css';
 
 function SearchBar({ onSubmit }) {
   const [term, setTerm] = useState("");
@@ -13,7 +14,7 @@ function SearchBar({ onSubmit }) {
   }
 
   return (
-    <div>
+    <div className="search-bar">
       <form onSubmit={handleFormSubmit}>
         <input value={term} onChange={handleChange} />
       </form>
